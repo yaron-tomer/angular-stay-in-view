04abfd7a65401e337d1dbc7b3f1ddfb8693a4654
yaront <yaron.tomer@xmpie.com>
2015-01-01 21:15:50 +0200

    basic functionality works

--- JavaScript.js
+++ JavaScript.js
@@ -8,11 +8,19 @@ module.directive("dynamicSearch", [function () {
     return{
 
         link: function (scope, element, attrs) {
- 
+            //var elementTop = element.offset().top;
+            var elementTop = 100;
             $(window).on("scroll", function () {
-                element.css('background-color', 'red');
-                var top =  $(window).scrollTop();
-                console.log("scrolled: "+top);
+                var scrollTop =  $(window).scrollTop();
+                console.log("scrolled: " + scrollTop + "   element:" + elementTop);
+                if (elementTop <= scrollTop) {
+                    element.addClass("search-fixed");
+                    $(".search-place-holder").css("padding-top", "130px");
+                }
+                else {
+                    element.removeClass("search-fixed");
+                    $(".search-place-holder").css("padding-top", "0px");
+                }
                 
             });
             element.on("mousedown", function () {
