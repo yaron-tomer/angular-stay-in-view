68436cf859f17849b3c078acb8b911ec76b9d57c
yaront <yaron.tomer@xmpie.com>
2015-01-01 23:53:48 +0200

    height independent directive
    issue: requires including a placeholder in the html

--- JavaScript.js
+++ JavaScript.js
@@ -8,14 +8,21 @@ module.directive("dynamicSearch", [function () {
     return{
 
         link: function (scope, element, attrs) {
-            //var elementTop = element.offset().top;
-            var elementTop = 100;
+            
+            var elementTop = undefined;
+            var elementHeight = undefined;
+            
+            //on scroll event
             $(window).on("scroll", function () {
+                if (elementTop == undefined) {
+                    elementTop = element.offset().top;
+                    elementHeight = element.outerHeight();
+                }
                 var scrollTop =  $(window).scrollTop();
                 console.log("scrolled: " + scrollTop + "   element:" + elementTop);
                 if (elementTop <= scrollTop) {
                     element.addClass("search-fixed");
-                    $(".search-place-holder").css("padding-top", "130px");
+                    $(".search-place-holder").css("padding-top",  elementHeight);
                 }
                 else {
                     element.removeClass("search-fixed");
@@ -23,6 +30,8 @@ module.directive("dynamicSearch", [function () {
                 }
                 
             });
+
+
             element.on("mousedown", function () {
                 console.log("mousedown");
             });
