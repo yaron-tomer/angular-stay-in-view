c0ba367b3bc024763f696fcb6861328f04374a97
yaront <yaron.tomer@xmpie.com>
2015-01-01 20:49:12 +0200

    captured scroll event and position

---
+++ JavaScript.js
@@ -0,0 +1,24 @@
+﻿/// <reference path="angular.js" />
+module = angular.module("searchy", []);
+
+
+
+
+module.directive("dynamicSearch", [function () {
+    return{
+
+        link: function (scope, element, attrs) {
+ 
+            $(window).on("scroll", function () {
+                element.css('background-color', 'red');
+                var top =  $(window).scrollTop();
+                console.log("scrolled: "+top);
+                
+            });
+            element.on("mousedown", function () {
+                console.log("mousedown");
+            });
+        } //link
+
+    };
+}]);
