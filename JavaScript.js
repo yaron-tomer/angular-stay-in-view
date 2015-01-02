8b1af81dd8f550f36886cf1009256ef029914845
yaront <yaron.tomer@xmpie.com>
2015-01-02 13:29:41 +0200

    Works a generic directive

--- JavaScript.js
+++ JavaScript.js
@@ -1,16 +1,30 @@
 ﻿/// <reference path="angular.js" />
 module = angular.module("searchy", []);
 
+module.factory("uidFactory", [function () {
+    var uid = 0;
+    var uidFactory = {};
+    uidFactory.create = function () {
+        uid = uid + 1;
+        return uid;
+    };
+    return uidFactory;
+}]);
 
 
-
-module.directive("dynamicSearch", [function () {
+module.directive("dynamicSearch", ["uidFactory", function (uidFactory) {
+    var myUidFactory = uidFactory;
     return{
-
+        scope:true,
         link: function (scope, element, attrs) {
             
             var elementTop = undefined;
             var elementHeight = undefined;
+            scope.placeholderId = "Sticky" + myUidFactory.create();
+
+            //append a placeholder
+            element.after("<div id=" + scope.placeholderId + "></div> ");
+
             
             //on scroll event
             $(window).on("scroll", function () {
@@ -22,11 +36,11 @@ module.directive("dynamicSearch", [function () {
                 console.log("scrolled: " + scrollTop + "   element:" + elementTop);
                 if (elementTop <= scrollTop) {
                     element.addClass("search-fixed");
-                    $(".search-place-holder").css("padding-top",  elementHeight);
+                    $("#" + scope.placeholderId).css("padding-top", elementHeight);
                 }
                 else {
                     element.removeClass("search-fixed");
-                    $(".search-place-holder").css("padding-top", "0px");
+                    $("#" + scope.placeholderId).css("padding-top", "0px");
                 }
                 
             });
