bd6d72bb0adcc0af0ac8d38d963267cbf2ebd1dd
yaront <yaron.tomer@xmpie.com>
2015-01-09 18:11:29 +0200

    Susceptible to change in height.
    issue: the back in view doesn't work.

--- angular-stay-in-view.js
+++ angular-stay-in-view.js
@@ -2,46 +2,69 @@
 module = angular.module("StayInView", []);
 
 //this factory is used to generate unique ids 
-module.factory("uidFactory", [function () {
+module.factory("stayInView.Factory", [function () {
     var uid = 0;
-    var uidFactory = {};
-    uidFactory.create = function () {
+    var factory = {};
+    var order = [];
+    factory.createUid = function () {
         uid = uid + 1;
         return uid;
     };
-    return uidFactory;
+    factory.order = function () {
+        return order;
+    }
+    return factory;
 }]);
 
 
-module.directive("stayInView", ["uidFactory",  function (uidFactory) {
-    return{
-        link: function (scope, element, attrs) {          
-            var elementTop = undefined; //dimension used to calculate location of element from top of page, so it can be calculated when it is scrolled out of view
-            var elementHeight = undefined;  //dimension used to know the height of the element, so that we can set the placeholder div to its height and prevent a jitter in the scroll
+module.directive("stayInView", ["stayInView.Factory", function (factory) {
+    return {
+        scope: true,
+        link: function (scope, element, attrs) {
+            var inScope = true;
             var stayClass = attrs.stayInView;   //defines the class used when scrolling out of view
             if (stayClass === "") {
                 stayClass = "stayInViewDefaultClass";
+                scope.stayClass = stayClass;
             }
 
             //append a placeholder
-            var placeholderId = "__StayInView__" + uidFactory.create();   //a unique id for the placeholder 
-            element.after("<div id=" + placeholderId + "></div> ");
-
+            var placeholderId = "__StayInView__" + factory.createUid();   //a unique id for the placeholder 
             
+            //add the placeholder element
+            element.after("<div id=" + placeholderId + "></div> ");
+            placeholderElement = $("#" + placeholderId);
+
+            var elementTop; //the element offset from the begining of the document
+
             //on scroll event
             $(window).on("scroll", function () {
-                if (elementTop == undefined) {  //calculate element location and height
+                var scrollTop = $(window).scrollTop();
+
+                if (inScope)
+                {
+                    //because the offset might change, constantly check its vertical  position relating the document
                     elementTop = element.offset().top;
-                    elementHeight = element.outerHeight();
+                    if (elementTop <= scrollTop) {  //if scrolled out of view
+                        element.addClass(stayClass);
+                        placeholderElement.css("padding-top", element.outerHeight());
+                        angular.forEach(factory.order(), function (el) {
+                            el.removeClass(el.scope().stayClass);
+                        });
+
+                        factory.order().push(element);
+                        inScope = false;
+                    }
+
                 }
-                var scrollTop =  $(window).scrollTop();
-                if (elementTop <= scrollTop) {  //if scrolled out of view
-                    element.addClass(stayClass);
-                    $("#" + placeholderId).css("padding-top", elementHeight);
-                }
-                else {  //if scrolled back in view
-                    element.removeClass(stayClass);
-                    $("#" + placeholderId).css("padding-top", "0px");
+                else {
+                    //var placeholderTop = placeholderElement.offset().top;
+                    //if (placeholderTop > scrollTop) {  //if scrolled back in view
+                    //    element.removeClass(stayClass);
+                    //    $("#" + placeholderId).css("padding-top", "0px");
+                    //    factory.order().pop();
+                    //    inScope = true;
+                    //}
                 }
                 
             }); // on scroll
