deaacbb7fe6a03deaf5f2c976e042dd2588d59fb
yaront <yaron.tomer@xmpie.com>
2015-01-10 17:00:02 +0200

    added comments and beautified

--- angular-stay-in-view.js
+++ angular-stay-in-view.js
@@ -3,18 +3,19 @@ module = angular.module("StayInView", []);
 
 //this factory is used to generate unique ids 
 module.factory("stayInView.Factory", [function () {
-    var uid = 0;
+
     var factory = {};
-    var order = [];
-    var totalStayHeight = 0;
+
+    //uid generator
+    var uid = 0;
     factory.createUid = function () {
         uid = uid + 1;
         return uid;
     };
-    factory.order = function () {
-        return order;
-    }
+   
 
+    //singleton property that calculates total height
+    var totalStayHeight = 0;
     factory.totalStayHeight = function (add) {
         if (add != undefined)
             totalStayHeight += add;
@@ -26,13 +27,11 @@ module.factory("stayInView.Factory", [function () {
 
 module.directive("stayInView", ["stayInView.Factory", function (factory) {
     return {
-        scope: true,
         link: function (scope, element, attrs) {
-            var inScope = true;
+            var inScope = true; //defines whether vertical went out of scope 
             var stayClass = attrs.stayInView;   //defines the class used when scrolling out of view
             if (stayClass === "") {
                 stayClass = "stayInViewDefaultClass";
-                scope.stayClass = stayClass;
             }
 
             //append a placeholder
@@ -46,38 +45,26 @@ module.directive("stayInView", ["stayInView.Factory", function (factory) {
 
             //on scroll event
             $(window).on("scroll", function () {
-                var scrollTop = $(window).scrollTop();
+                var scrollTop = $(window).scrollTop();  //scroll location
 
                 if (inScope)
                 {
                     //because the offset might change, constantly check its vertical  position relating the document
                     elementTop = element.offset().top;
                     if (elementTop <= scrollTop + factory.totalStayHeight()) {  //if scrolled out of view
-                        placeholderElement.css("padding-top", element.outerHeight());
+                        placeholderElement.css("padding-top", element.outerHeight()); //make the placeholder replace the same vertical height as the original element
                         element.addClass(stayClass)
-                            .css("top", factory.totalStayHeight());
-                        //angular.forEach(factory.order(), function (el) {
-                        //    el.removeClass(el.scope().stayClass);
-                        //});
-
+                            .css("top", factory.totalStayHeight()); //make the original element stock below the other elements forced to stay in view
                         factory.totalStayHeight(element.outerHeight());
-                        factory.order().push(element);
                         inScope = false;
                     }
 
                 }
-                else if (placeholderElement.offset().top - scrollTop + placeholderElement.outerHeight() > factory.totalStayHeight()) {
-                    placeholderElement.css("padding-top", 0);
-                    element.removeClass(stayClass);
-                    factory.totalStayHeight(-element.outerHeight());
+                else if (placeholderElement.offset().top - scrollTop + placeholderElement.outerHeight() > factory.totalStayHeight()) { //if scrolled back in view
+                    placeholderElement.css("padding-top", 0); //eliminate the placholder presense
+                    element.removeClass(stayClass); //restore the original class -- remove it from teh stack
+                    factory.totalStayHeight(-element.outerHeight());    //reduce the stack height
                     inScope = true;
-                    //var placeholderTop = placeholderElement.offset().top;
-                    //if (placeholderTop > scrollTop) {  //if scrolled back in view
-                    //    element.removeClass(stayClass);
-                    //    $("#" + placeholderId).css("padding-top", "0px");
-                    //    factory.order().pop();
-                    //    inScope = true;
-                    //}
                 }
                 
             }); // on scroll
